a6ad802ff03c0ece0999bf54cb173a18c4782254
yuuuba <nsabu45@gmail.com>
2023-01-17 16:25:07 +0900

    devise nameカラム実装まで#5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,18 +2,4 @@ class UsersController < ApplicationController
   def show
   end
 
-  def new
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'tops#index'
   devise_for :users
-  resources :users, only:[:show, :edit, :update, :destroy]
   resources :ondemands
 end
