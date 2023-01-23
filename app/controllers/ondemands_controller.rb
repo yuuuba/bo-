472d1bd861b3cad4d077d63e572c09e991591a36
yuuuba <nsabu45@gmail.com>
2023-01-23 13:42:41 +0900

    new動作まで#7

--- app/controllers/ondemands_controller.rb
+++ app/controllers/ondemands_controller.rb
@@ -1,4 +1,6 @@
 class OndemandsController < ApplicationController
+  before_action :set_ondemand, only: %i[ show edit update destroy ]
+
   def index
   end
 
@@ -6,12 +8,18 @@ class OndemandsController < ApplicationController
   end
 
   def new
+    @ondemand = Ondemand.new
   end
 
   def edit
   end
 
   def create
+    @ondemand = Ondemand.new(ondemand_params)
+
+    if @ondemand.save!
+      redirect_to root_path
+    end
   end
 
   def update
@@ -19,4 +27,14 @@ class OndemandsController < ApplicationController
 
   def destroy
   end
+
+  private
+    def set_ondemand
+      @ondemand = Ondemand.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def ondemand_params
+      params.require(:ondemand).permit(:title, :body)
+    end
 end
