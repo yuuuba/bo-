3caaac6261564a0c1167cdef5b3a766fb3a366b1
yuuuba <nsabu45@gmail.com>
2023-02-12 20:48:18 +0900

    カテゴリseed修正#7

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,10 @@
 #   )
 # end
 
+City.destroy_all
+Prefecture.destroy_all
+OndemandCategory.destroy_all
+
 require 'csv'
 
 # CSV読み込み
@@ -40,4 +44,56 @@ end
 cities_list.each do |prefecture, city|
   prefecture = Prefecture.find_by(name: prefecture)
   prefecture.cities.create(name: city)
+end
+
+%w[ネット].each do |name|
+  OndemandCategory.create!(
+    name: name,
+    parent_id: "",
+  )
+end
+
+%w[スポーツ 音楽 ゲーム ボードゲーム アウトドア 交流].each do |name|
+  OndemandCategory.create!(
+    name: name,
+    parent_id: "1",
+  )
+end
+
+%w[野球 ソフトボール サッカー フットサル バスケ バレー バトミントン ハンドボール 卓球 ドッジボール ゲートボール ボウリング
+  ビリヤード ランニング・マラソン 水泳 フィットネス ヨガ 体操 サイクリング ダーツ テニス ゴルフ バスケット ラグビー アメフト 格闘技
+  ボクシング キックボクシング 柔道/柔術 ウィンタースポーツ スキー スノーボード フィギュアスケート フィールドホッケー アイスホッケー
+  相撲 柔道 剣道 空手 フェンシング ムエタイレスリング 総合格闘技 テコンドー モータースポーツF1 水泳 サーフィン その他スポーツ].each do |name|
+  OndemandCategory.create!(
+    name: name,
+    parent_id: "2",
+  )
+end
+
+%w[バンド ギター/アコギ ピアノ ボーカル JAZZ カラオケ].each do |name|
+  OndemandCategory.create!(
+    name: name,
+    parent_id: "3",
+  )
+end
+
+%w[ボードゲーム 囲碁 将棋 チェス 麻雀 その他].each do |name|
+  OndemandCategory.create!(
+    name: name,
+    parent_id: "5",
+  )
+end
+
+%w[キャンプ つり 山登り].each do |name|
+  OndemandCategory.create!(
+    name: name,
+    parent_id: "6",
+  )
+end
+
+%w[グルメ/食べ歩き カフェ巡り 飲み会 ナイトクラブ 自分磨き ダンス].each do |name|
+  OndemandCategory.create!(
+    name: name,
+    parent_id: "7",
+  )
 end
