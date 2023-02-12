854ff30283d52a9fa6439799820628bc6a79aa13
yuuuba <nsabu45@gmail.com>
2023-02-12 20:46:48 +0900

    自己結合リレーション#7

--- app/models/ondemand_category.rb
+++ app/models/ondemand_category.rb
@@ -1,2 +1,6 @@
 class OndemandCategory < ApplicationRecord
+  has_many :children, class_name: "OndemandCategory",
+                      foreign_key: "parent_id"
+
+  belongs_to :ondemand, class_name: "OndemandCategory", optional: true
 end
