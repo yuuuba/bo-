e089a1d8672458042b721a86f083159265d4335f
yuuuba <nsabu45@gmail.com>
2023-03-06 20:50:56 +0900

    feat: カテゴリ表示するまで#16

--- app/models/ondemand_category.rb
+++ app/models/ondemand_category.rb
@@ -5,4 +5,17 @@ class OndemandCategory < ApplicationRecord
   belongs_to :parent, class_name: "OndemandCategory", optional: true
 
   scope :root_categories, -> { where(parent_id: [nil, '']) }
+
+  class << self
+    def to_group
+      hash_grouped_categories =
+        root_categories.map do |root_category|
+          {
+            root_category.name.to_sym => root_category.children.map(&:name)
+          }
+        end
+
+      hash_grouped_categories.reduce({}) { |ret, hash| ret.merge(hash) }
+    end
+  end
 end
