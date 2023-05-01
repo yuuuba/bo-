5254542134a047e4503c6a5523e76632c0542d4e
yu <nsabu45@gmail.com>
2023-05-01 22:09:36 +0900

    style:テーマ、タイポグラフィ追加#28

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,29 @@
 module.exports = {
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#97ed80",
+          "secondary": "#dd836c",
+          "accent": "#efb5fc",
+          "neutral": "#282531",
+          "base-100": "#FAF9FB",
+          "info": "#83A8F6",
+          "success": "#47D17E",
+          "warning": "#F4CD4E",
+          "error": "#FB4655",
+        },
+      },
+    ],
+  },
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
   ],
-  plugins: [require("daisyui")],
+  plugins: [
+    require("daisyui"),
+    require('@tailwindcss/typography')
+  ],
 }
